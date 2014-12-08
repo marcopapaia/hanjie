c4d7d9ba41825a0e44ffde85f2873e8dad7f324c
ollocip <nicolas.francois@viacesi.fr>
2014-12-08 09:25:39 +0000

    Menu 2.0

--- main.c
+++ main.c
@@ -3,40 +3,61 @@
 
 int main()
 {
-    int a,b;
+    int a,b,c;
+    int numDiff,TailleResultats;
+    char pseudo[50];
 
-    do {
+
     printf(" Bienvenue pour jouer a une partie de Hanjie \n\n Que voulez vous faire ?\n");
     printf("\t1=Jouer une partie\n\t2=Voir l'historique des parties\n\t3=Quitter  " );
     scanf("\n%d",&a);
 
         if (a==1) {
-            system("clear");
 
-            do {
+
+
+            system("clear");
             printf(" Menu de partie \n ");
             printf("\t1=Nouvelle partie\n\t2=Charger une partie\n\t3=Revenir au menu \n " );
             scanf("\n%d",&b);
 
             switch (b) {
                 case 1 :{
+
                     system("clear");
-                    //hanjie();
-                }
-                case 2 : {
-                    system("clear");
+                    printf("Entrez votre pseudo:\n ");
+                    scanf("%s",pseudo);
+                    printf("Quelle difficulté du niveau voulez vous ?\n\t1=FACILE\n\t2=MOYEN\n\t3=DIFFICILE\n");
+                    scanf ("%d",&numDiff);
+                    printf("Quelle taille de d'affichage des resultats voulez vous ? \n\t1=5*3\n\t2=7*4\n\t3=9*5\n\t4=11*6\n ");
+                    scanf ("%d",&TailleResultats);
+
+
+
+
+
+                    }
+                    break;
+                    case 2 : {
+                        system("clear");
                     //sauvegarde();
-                }
+                            }
+                    case 3: {
+                    system("clear");
+                    return main();
+                    }
+                    break;
+
+
+
 
                 }
 
                 }
-                while (b>3);
-                system("clear");
-                return main();
 
 
-                };
+
+
         if (a==2){
             system("clear");
 
@@ -47,12 +68,10 @@ int main()
             return 0;
             }
 
-            }
 
-        while (a>3);
 
-    system("PAUSE");
-    return 0 ;
+
+
 }
 
 
