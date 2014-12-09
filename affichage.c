fb50a6295eae1574963286e7845eec718aeb13ce
ItsWhiteSome <yasledur@hotmail.fr>
2014-12-09 09:23:29 +0100

    Nouveautés

--- affichage.c
+++ affichage.c
@@ -1,23 +1,46 @@
-void affichage()
+#include "structures.h"
+
+void affichage (Difficulte diff)
 {
+    int x,y;
+    int tab [x][y];
+    int i,j;
 
-int tab[8][4];
-int i;
-int j;
+if (diff == 0)
+{
+    x=5;
+    y=5;
+};
 
-    for (i = 0; i <= 8; i++)
+if (diff == 1)
+{
+    x=10;
+    y=10;
+};
+
+if (diff == 2)
+{
+    x=15;
+    y=15;
+};
+
+
+    for (j = 0; j < x; j++)
+    {
+        for(i = 0; i < y; i++)
         {
-            for (j = 0; j <= 4; j++)
-                {
-                    printf(" ");
-                    printf("%c%c%c", 0xE2, 0x96, 0x81);
-                    printf(" \n");
-                    printf("%c%c%c", 0xE2, 0x96, 0x95);
-                    printf("%c%c%c", 0xE2, 0x96, 0x88);
-                    printf("%c%c%c\n", 0xE2, 0x96, 0x8f);
-                    printf(" ");
-                    printf("%c%c%c", 0xE2, 0x96, 0x94);
-                }
-        }
+            if (tab[i][j] == 0)
+            {
+                printf("%c%c%c ", 0xE2, 0x96, 0xA9);
+            }
 
+            else
+            {
+                printf("%c%c%c ", 0xE2, 0x96, 0xA1);
+            }
+
+        }
+        printf("\n");
+
+    }
 }
