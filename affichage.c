b4aa98f97f7fceac9dfc693979a2dbb44d6f5ca0
ItsWhiteSome <yasledur@hotmail.fr>
2014-12-09 15:43:00 +0100

    Yes!

--- affichage.c
+++ affichage.c
@@ -6,11 +6,17 @@ void affichage (Cases * niveau)
     int x,y;
     int i,j;
 
+    char indicesLigne[5][15] = {"\n 4  ","1 2 "," 1  ","2 1 "," 2  "};
+    char indicesColonne[5][25] = {"\n    1 2 1 4 2\n      2 1 "};
+    printf("%s", indicesColonne);
+
     for (i = 0; i < niveau->y; i++)
     {
+                printf("%s", indicesLigne[i]);
         for(j = 0; j < niveau->x; j++)
         {
-            if (niveau->grille[i][j] == '0')
+
+            if (niveau->grille[i][j] == '1')
             {
                 printf("%c%c%c ", 0xE2, 0x96, 0xA9);
             }
@@ -19,9 +25,17 @@ void affichage (Cases * niveau)
             {
                 printf("%c%c%c ", 0xE2, 0x96, 0xA1);
             }
-
         }
         printf("\n");
-
     }
+
+}
+
+void score()
+{
+
+
+
+
+
 }
