750029c59a81d1c86b6f43f053401886a2543f28
Azerom <az3rom@gmail.com>
2014-12-09 15:36:45 +0100

    Ajout du fichier fonctions.c (et son .h) avec la fonction SystemPause

---
+++ fonctions.h
@@ -0,0 +1,10 @@
+#ifndef FONCTIONS_H_INCLUDED
+#define FONCTIONS_H_INCLUDED
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "structures.h"
+
+void SystemPause();
+
+#endif // FICHIERS_H_INCLUDED
