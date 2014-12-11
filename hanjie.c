be30dd26e8c3b2ea30321040ae997ce737428dd4
ollocip <nicolas.francois@viacesi.fr>
2014-12-11 10:59:32 +0000

    Correction

--- hanjie.c
+++ hanjie.c
@@ -12,10 +12,13 @@ char lettrem[16]={'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o'};
 char chiffre[16]={'1','2','3','4','5','6','7','8','9','10','11','12','13','14','15'};
 
 do{
+    affichage(partie);
     printf("Quelle case voulez vous selectionner ? ");
-    scanf ("%s",&position1);4
+    scanf ("%s",&position1);
 
-//Recherche pour savoir à quelle position se trouve la premiere lettre (en majuscule ou minuscule)
+
+//Recherche pour savoir à quelle position se trouve la premiere lettre (en majuscule ou minuscule)//
+
     POS= -1;
     for (i=0;(i<partie->actuel.x)&&(POS==-1);i++){
         if (lettreM[i]==position1[0])
